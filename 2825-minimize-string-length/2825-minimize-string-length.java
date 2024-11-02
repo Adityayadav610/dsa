ba3ddbfc31c5b6368965628b9e5cbcbf2502a6a9
Aditya Yadav <158409691+Adityayadav610@users.noreply.github.com>
2024-11-02 10:51:02 +0530

    Time: 7 ms (66.36%), Space: 45.1 MB (42.69%) - LeetHub

---
+++ 2825-minimize-string-length/2825-minimize-string-length.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minimizedStringLength(String s) {
+         HashSet<Character> uniqueChars = new HashSet<>();  
+        
+        for (char c : s.toCharArray()) {  
+            uniqueChars.add(c);  
+        }  
+        
+        return uniqueChars.size();  
+        
+    }
+}
